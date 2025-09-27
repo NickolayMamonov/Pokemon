dd4ca3f73e4bdb894ec33754ba044eb64dd2da08
Whysoezzy <n.mamonovv@gmail.com>
2025-09-27 21:58:12 +0300

    add coil dependency

--- core/core-uikit/src/main/java/dev/whysoezzy/core_uikit/components/cards/PokemonCard.kt
+++ core/core-uikit/src/main/java/dev/whysoezzy/core_uikit/components/cards/PokemonCard.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import dev.whysoezzy.core_common.extensions.toDisplayName
 import dev.whysoezzy.core_common.extensions.toTypeColor
 import dev.whysoezzy.core_uikit.components.stats.StatItem
